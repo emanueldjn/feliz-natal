afb71ed79dce1b1b1f89c052e37c1f15a1800a48
Emanuel <emanuelnas29@gmail.com>
2025-08-10 19:59:36 -0300

    imgs

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  css: {
+    modules: {
+      scopeBehaviour: 'global',
+    },
+  },
+  base: '/', // deixe '/' se o site estiver na raiz
+});
